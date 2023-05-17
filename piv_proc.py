1a0d68ed86b3943102a5a072cf984b27d1784918
11matic <matic.tomazevic@gmail.com>
2023-05-17 22:10:50 +0200

    comments

--- piv_proc.py
+++ piv_proc.py
@@ -2,19 +2,21 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+#Loading images
 img_src = cv.imread("images/alpha15_Diff/B00001.tif")
-img_src_cal = cv.imread("images/Calibration_02/B00001.tif")
+img_src_cal = cv.imread("images/Calibration_02/B00001.tif")[1236:, :]
 
-
+#Spliting the image in two
 img_a = img_src[:1236, :]
 img_b = img_src[1236:, :]
-img_cal = cv.addWeighted(img_src_cal, 0, img_src_cal, 5, 4)
+
+#Correcting exposure and brightnes and combining both images
+img_cal = cv.addWeighted(img_src_cal, 2.5, img_src_cal, 2.5, 4)
 img = cv.addWeighted(img_a, 100, img_b, 100, 30)
 
+#Showing images
 plt.imshow(img_cal)
 plt.show()
-
 
 plt.imshow(img)
 plt.show()
