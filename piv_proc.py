c6cdcee8eec052677893f5801a0382244cabe645
11matic <matic.tomazevic@gmail.com>
2023-05-28 14:54:34 +0200

    mask implementation

--- piv_proc.py
+++ piv_proc.py
@@ -6,9 +6,11 @@ from scipy import ndimage
 import math
 
 # Defining the variables
-Int_Win = 32               # Interrogation window size in pixles
-Overlap = 50               # Overlap of interrogation windows in %
-dt = 72*10**-6
+AoA = 15           # airfoil angle of attack (0, 5 or 15)
+mask_on = 1          # 0 - mask off; 1 - mask on
+Int_Win = 32      # Interrogation window size in pixles
+Overlap = 50      # Overlap of interrogation windows in %
+dt = 72*10**-6    # Time step between two frames
 
 # Calibration
 p1 = [300.8, 317.0]
@@ -17,8 +19,11 @@ pix_dist = math.dist(p1, p2)
 pix_vel = 0.11/(pix_dist*dt) # speed per 1px of movement
 
 #Loading images
-img_src = cv.imread("images/alpha15_Diff/B00001.tif", -1)
+img_src = cv.imread("images/Alpha15_Diff/B00001.tif", -1)
 img_src_cal = cv.imread("images/Calibration_02/B00001.tif", -1)[1236:, :]
+mask0 = cv.imread("images/mask0.tif")
+mask5 = cv.imread("images/mask5.tif")
+mask15 = cv.imread("images/mask15.tif")
 
 #Spliting the image in two
 yMax = img_src.shape[1] - img_src.shape[1]%Int_Win # Clipping the end
@@ -40,10 +45,19 @@ def slice(image):
 
 slice_a = slice(img_a)
 slice_b = slice(img_b)
+if AoA == 0:
+    slice_m0 = slice(mask0)
+elif AoA == 5:
+    slice_m0 = slice(mask5)
+elif AoA == 15:
+    slice_m0 = slice(mask15)
+else:
+    print("Angle of attack input error")
 
 # Arrays with x and y velocities
 dys = np.zeros((int(xMax/((1-(Overlap/100))*Int_Win)), int(yMax/((1-(Overlap/100))*Int_Win))))
 dxs = np.zeros((int(xMax/((1-(Overlap/100))*Int_Win)), int(yMax/((1-(Overlap/100))*Int_Win))))
+mask_arr = np.zeros((int(xMax/((1-(Overlap/100))*Int_Win)), int(yMax/((1-(Overlap/100))*Int_Win))))
 
 # The cross correlation function
 def cross_corr(sl_img_a, sl_img_b, x, y):
@@ -61,6 +75,21 @@ for x in range(slice_a.shape[0]):
         dys[x][y] = dy
         dxs[x][y] = -dx
 
+# Apply mask
+if mask_on == 1:
+    for i in range(slice_m0.shape[0]):
+        for j in range(slice_m0.shape[1]):
+            count = 0
+            for k in range(slice_m0[i][j].shape[0]):
+                for l in range(slice_m0[i][j].shape[1]):
+                    if max(slice_m0[i][j][k][l]) == 255:
+                        count += 1
+            if count>=1:
+                dys[i][j] = 0
+                dxs[i][j] = 0
+elif mask_on != 0:
+    print("Mask on input error")
+
 vect = np.sqrt(dxs**2 + dys**2)*pix_vel
 
 # Plot the vector field
@@ -72,14 +101,15 @@ plt.show()
 
 # Plot the velocity magnitude
 plt.imshow(vect[:-2, :-2], vmin=0, vmax=14, cmap="jet")
+plt.colorbar()
 plt.show()
 
 
 # Showing images
 # plt.imshow(img_src_cal, cmap="gray", vmin=0, vmax=10000)
 # plt.show()
-# plt.imshow(img_a, vmin=0, vmax = 300, cmap="gray")
-# plt.show()
+plt.imshow(img_a, vmin=0, vmax = 300, cmap="gray")
+plt.show()
 # plt.imshow(img_overlay, vmin=0, vmax = 300, cmap="gray")
 # plt.show()
 
